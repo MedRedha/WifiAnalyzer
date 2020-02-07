52ea77d1c3cc0def459160170349e94028a9872b
Med Redha <redha.dirac@gmail.com>
2020-02-07 16:26:39 +0100

    Initial commit

---
+++ WifiAnalyzer/ANAppDelegate.h
@@ -0,0 +1,32 @@
+//
+//  ANAppDelegate.h
+//  WifiAnalyzer
+//
+//  Created by Alex Nichol on 7/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "CarbonAppProcess.h"
+
+typedef enum {
+    ANViewSlideDirectionForward,
+    ANViewSlideDirectionBackward
+} ANViewSlideDirection;
+
+@class ANListView;
+
+@interface ANAppDelegate : NSObject <NSApplicationDelegate> {
+    NSView * activeView;
+    NSView * nextView;
+    BOOL animating;
+    ANListView * networkList;
+}
+
+@property (assign) IBOutlet NSWindow * window;
+
+- (void)pushView:(NSView *)view direction:(ANViewSlideDirection)direction;
+- (void)animationComplete;
+- (void)showNetworkList;
+
+@end
